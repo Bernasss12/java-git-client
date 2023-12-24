3594e5243b92459074a9bfe24c842c622f5f9282
Bernardo Antunes <bernasss12@gmail.com>
2023-12-24 01:19:38 +0000

    remove debug print, oops

--- src/main/java/dev/bernasss12/git/Main.java
+++ src/main/java/dev/bernasss12/git/Main.java
@@ -5,7 +5,6 @@ import picocli.CommandLine;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("java.version"));
         new CommandLine(new GitCommand()).execute(args);
     }
 }
